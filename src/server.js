9a5c7aa63577d79d7175ad2e6be425854a630ff1
Tobi Emmanuel <tobiemmanuel19@gmail.com>
2025-11-06 21:01:40 +0100

    remove vhost

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 // src/server.js
+
+/* initial setup
+
 import express from "express";
 import vhost from "vhost";
 import mainApp from "./mainApp.js";
@@ -26,3 +29,31 @@ console.log("Views folder:", app.get("views"));
     process.exit(1);
   }
 })();
+
+*/
+
+import express from "express";
+import mainApp from "./mainApp.js";
+import adminApp from "./adminApp.js";
+import { APP_CONFIG } from "./config/config.js";
+
+const app = express();
+
+// TEMP: disable vhost for deployment
+app.use("/admin", adminApp);
+app.use("/", mainApp);
+
+console.log("Views folder:", app.get("views"));
+
+(async () => {
+  try {
+    await APP_CONFIG.init();
+    app.listen(APP_CONFIG.ENV.PORT, () => {
+      APP_CONFIG.LOGGER.info(`Nutrismart running on port ${APP_CONFIG.ENV.PORT}`);
+    });
+  } catch (err) {
+    console.error("Startup failed:", err);
+    process.exit(1);
+  }
+})();
+
